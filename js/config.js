14da2a2080eb569ca93bdb9ffb94b01e48b64344
Ovopacific Deploy <deploy@ovopacific.com>
2026-04-24 09:44:55 -0500

    Update Google Apps Script URL (Latest Deployment)

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 window.GARAJE_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbw_twsB80wVdLofdq0f0RylymlfjNhllWHwdnet8F83MqaNBwS4BG6ROqkP9M1lShQC/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbx2oGr5sFCmfHS4I6hjFUa7bL5OUMw8GADSwwvMppqKUFE1g41VRg8QAYXMrdPiELxB/exec",
   REQUEST_TIMEOUT_MS: 15000
 };
